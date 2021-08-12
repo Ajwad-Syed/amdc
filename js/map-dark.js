d0f0c33676b874eb6804ffa663f6c2f2deeb7a74
Syed Ale <syed.ale@ncri.local>
2021-08-13 00:58:49 +0500

    more changes

--- js/map-dark.js
+++ js/map-dark.js
@@ -11,7 +11,7 @@ function init() {
         zoom: 16,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.722330, -73.992905), // You Address Here
+        center: new google.maps.LatLng(25.314030041818068, 55.37288479630839), // You Address Here
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -21,7 +21,7 @@ function init() {
         // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles:
-            [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+            [{ "stylers": [{ "hue": "#ff1a00" }, { "invert_lightness": true }, { "saturation": -100 }, { "lightness": 33 }, { "gamma": 0.5 }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#2D333C" }] }]
     };
 
     // Get the HTML DOM element that will contain your map
@@ -33,7 +33,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/misc/map-marker-light.png';
-    var myLatLng = new google.maps.LatLng(40.722330, -73.992905); // You Address Here
+    var myLatLng = new google.maps.LatLng(25.314030041818068, 55.37288479630839); // You Address Here
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: mapdark,
